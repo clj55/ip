169369bc537f950d66187083a3426c21578d31eb
clj55 <103747880+clj55@users.noreply.github.com>
2025-01-24 12:18:14 +0800

    Level-0

---
+++ src/main/java/Psyduck.java
@@ -0,0 +1,10 @@
+public class Psyduck {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! I'm Psyduck");
+        System.out.println("PSYDUCK?");
+        System.out.println("--------------------------------------------");
+        System.out.println("PSYYYYDUCKKKK");
+        System.out.println("--------------------------------------------");
+    }
+}
