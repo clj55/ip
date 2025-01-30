2efee21391f7bcb48f3c5c3035304dc6648f1dd9
clj55 <103747880+clj55@users.noreply.github.com>
2025-01-30 23:26:41 +0800

    add echo

--- src/main/java/Psyduck.java
+++ src/main/java/Psyduck.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Psyduck {
     public static void main(String[] args) {
-        
+        Scanner scanObj = new Scanner(System.in);
         System.out.println("Hello! I'm Psyduck");
         System.out.println("PSYDUCK?");
         System.out.println("--------------------------------------------");
+        String userInput = scanObj.nextLine();
+        System.out.println("--------------------------------------------");
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            System.out.println("--------------------------------------------");
+            userInput = scanObj.nextLine();
+            System.out.println("--------------------------------------------");
+        }
         System.out.println("PSYYYYDUCKKKK");
         System.out.println("--------------------------------------------");
+        
     }
 }
