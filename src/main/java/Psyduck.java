df5309f8f90593205134d87430eed4c54a1688e4
clj55 <103747880+clj55@users.noreply.github.com>
2025-01-30 23:36:54 +0800

    add user-psyduck roles

--- src/main/java/Psyduck.java
+++ src/main/java/Psyduck.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 public class Psyduck {
+    public static void printDashes () {
+        System.out.println("--------------------------------------------");
+    }
     public static void main(String[] args) {
         Scanner scanObj = new Scanner(System.in);
         System.out.println("Hello! I'm Psyduck");
         System.out.println("PSYDUCK?");
-        System.out.println("--------------------------------------------");
+        printDashes();
+        System.out.print("Me: ");
         String userInput = scanObj.nextLine();
-        System.out.println("--------------------------------------------");
+        printDashes();
         while (!userInput.equals("bye")) {
+            System.out.print("Psyduck: ");
             System.out.println(userInput);
-            System.out.println("--------------------------------------------");
+            printDashes();
+            System.out.print("Me: ");
             userInput = scanObj.nextLine();
-            System.out.println("--------------------------------------------");
+            printDashes();
         }
         System.out.println("PSYYYYDUCKKKK");
-        System.out.println("--------------------------------------------");
+        printDashes();
         
     }
 }
