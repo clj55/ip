317badf8bda8bd8a01b30a846fd17c06c2055a33
clj55 <103747880+clj55@users.noreply.github.com>
2025-01-31 00:29:59 +0800

    add mark as done

--- src/main/java/Psyduck.java
+++ src/main/java/Psyduck.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 public class Psyduck {
     public static void printDashes () {
         System.out.println("--------------------------------------------");
     }
     public static void main(String[] args) {
         Scanner scanObj = new Scanner(System.in);
+        List<String> storage = new ArrayList<>();
+        List<Boolean> isDone = new ArrayList<>();
         System.out.println("Hello! I'm Psyduck");
         System.out.println("PSYDUCK?");
         printDashes();
@@ -12,9 +14,60 @@ public class Psyduck {
         String userInput = scanObj.nextLine();
         printDashes();
         while (!userInput.equals("bye")) {
-            System.out.print("Psyduck: ");
-            System.out.println(userInput);
-            printDashes();
+            if (userInput.equals("list")) {
+                int num = 1;
+                for (String s : storage) {
+                    System.out.print(num + ".[");
+                    if (isDone.get(num - 1)) {
+                        System.out.print("X");
+                    } else {
+                        System.out.print(" ");
+                    }
+                    System.out.println("] " + s);
+                    num += 1;
+                }
+            } else if (userInput.startsWith("unmark")) {
+                
+                String sNum = userInput.substring(7); 
+                // System.out.println(sNum);
+                try {
+                    int iNum = Integer.parseInt(sNum);
+                    if ((iNum > isDone.size()) || (iNum <= 0)) {
+                        System.out.println("PSYDUCK ANGRY: PSYDUCK WANT NUMBER IN RANGE");
+                    } else {
+                        System.out.println("Psyduck is NOT impressed");
+                        iNum -= 1; // adjust to start with zero index
+                        isDone.set(iNum, false);
+                        System.out.println("[ ] " + storage.get(iNum));
+                    }
+                } catch (NumberFormatException e){
+                    System.out.println("PSYDUCK ANGRY: PSYDUCK WANT NUMBERS");
+                }
+
+            } else if (userInput.startsWith("mark")) {
+                
+                String sNum = userInput.substring(5); 
+                // System.out.println(sNum);
+                try {
+                    int iNum = Integer.parseInt(sNum);
+                    if ((iNum > isDone.size()) || iNum <= 0) {
+                        System.out.println("quacker not in list");
+                    } else {
+                        System.out.println("Psyduck is impressed");
+                        iNum -= 1; // adjust to start with zero index
+                        isDone.set(iNum, true);
+                        System.out.println("[X] " + storage.get(iNum));
+                    }
+                } catch (NumberFormatException e){
+                    System.out.println("PSYDUCK ANGRY: PSYDUCK WANT NUMBERS");
+                }
+            } else {
+                storage.add(userInput);
+                isDone.add(false);
+                System.out.print("Psyduck added: ");
+                System.out.println(userInput);    
+            }
+            printDashes();  
             System.out.print("Me: ");
             userInput = scanObj.nextLine();
             printDashes();
