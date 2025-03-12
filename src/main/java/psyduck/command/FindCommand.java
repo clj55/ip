0b40b311826cfe6957156f8fc07d45f7666f9d9d
clj55 <103747880+clj55@users.noreply.github.com>
2025-03-12 12:09:43 +0800

    Add Find Command and Catch when user puts too many datetimes in Event or Deadline

---
+++ src/main/java/psyduck/command/FindCommand.java
@@ -0,0 +1,4 @@
+package psyduck.command;
+
+public class FindCommand {
+}
---
+++ src/main/java/psyduck/exception/ExcessArgsException.java
@@ -0,0 +1,7 @@
+package psyduck.exception;
+
+public class ExcessArgsException extends RuntimeException {
+  public ExcessArgsException(String message) {
+    super(message);
+  }
+}
